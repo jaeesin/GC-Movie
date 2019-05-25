4a4322d20f58bbff29fe38b43edf3db41b5e3543
KimYoonho Aaron <youknow.yoonho@gmail.com>
2019-05-25 10:45:27 +0900

    Retrofit done

---
+++ app/src/main/java/com/youknow/gcmovie/data/source/TmdbService.java
@@ -0,0 +1,17 @@
+package com.youknow.gcmovie.data.source;
+
+import com.youknow.gcmovie.data.model.MoviesResp;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface TmdbService {
+
+    @GET("/3/movie/now_playing")
+    Call<MoviesResp> getNowPlayings(@Query("api_key") String apiKey);
+
+    @GET("/3/movie/upcoming")
+    Call<MoviesResp> getUpcomings(@Query("api_key") String apiKey);
+
+}
---
+++ app/src/main/java/com/youknow/gcmovie/data/source/TmdbServiceProvider.java
@@ -0,0 +1,26 @@
+package com.youknow.gcmovie.data.source;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class TmdbServiceProvider {
+
+    private static TmdbService tmdbService = null;
+
+    private TmdbServiceProvider() {
+    }
+
+    public static synchronized TmdbService getService() {
+        if (tmdbService == null) {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://api.themoviedb.org/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            tmdbService = retrofit.create(TmdbService.class);
+        }
+
+        return tmdbService;
+    }
+
+}
