b227cac4282b48d20d655c8b7b45550647812c1f
KimYoonho Aaron <youknow.yoonho@gmail.com>
2019-05-25 10:05:09 +0900

    Splash animation done!

--- app/src/main/java/com/youknow/gcmovie/ui/main/MainActivity.java
+++ app/src/main/java/com/youknow/gcmovie/ui/main/MainActivity.java
@@ -1,60 +1,17 @@
 package com.youknow.gcmovie.ui.main;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.youknow.gcmovie.R;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
 
-    public static final String MOVIE_ID = "movie_id";
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final ViewPager mainViewPager = findViewById(R.id.mainViewPager);
-        mainViewPager.setAdapter(new BottomNavigationAdapter(getSupportFragmentManager()));
-
-        final BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavigation);
-        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.action_now_playing:
-                        mainViewPager.setCurrentItem(0);
-                        return true;
-                    case R.id.action_upcoming:
-                        mainViewPager.setCurrentItem(1);
-                        return true;
-                    default:
-                        return false;
-                }
-            }
-        });
-
-        mainViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                bottomNavigation.getMenu().getItem(position).setChecked(true);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
     }
 }
