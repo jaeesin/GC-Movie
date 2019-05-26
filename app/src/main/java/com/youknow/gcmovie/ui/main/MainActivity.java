a0105f0effb4b62e0e054b1aa4e351c319b05079
KimYoonho Aaron <youknow.yoonho@gmail.com>
2019-05-26 10:02:52 +0900

    불필요 코드 삭제

--- app/src/main/java/com/youknow/gcmovie/ui/main/MainActivity.java
+++ app/src/main/java/com/youknow/gcmovie/ui/main/MainActivity.java
@@ -1,13 +1,9 @@
 package com.youknow.gcmovie.ui.main;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import com.youknow.gcmovie.R;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,21 +13,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavigation);
-        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.action_now_playing:
-                        return true;
-                    case R.id.action_upcoming:
-                        return true;
-                    default:
-                        return false;
-                }
-            }
-        });
-
     }
 }
